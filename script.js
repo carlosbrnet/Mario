288545c0e4eb7b0a08c3ef48386b37368bf90a49
carlosbrnet <carlosbrnet@hotmail.com>
2024-07-29 15:59:49 -0300

    escondendo o formulário

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const formulario = document.querySelector(".formulario-caixa");
 const fundoEscuro = document.querySelector(".mascara-formulario");
  
 
-function mostrarForm() {
+function mostrarForm() {  // Evento aplicado no BOTÃO
   formulario.style.left = "50%";
   formulario.style.transform = "translateX(-50%)";
   fundoEscuro.style.visibility = "visible";
 }
 
-function esconderForm() { 
+function esconderForm() {  // Evento aplicado no FUNDO ESCURO
   formulario.style.left = "-200px"; 
   formulario.style.transform = "translateX(0)";
   fundoEscuro.style.visibility = "hidden";
+  
   
 }
